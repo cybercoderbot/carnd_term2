a3decb840190b05af6307a8170e4106102498f2a
Yuheng Wang <yxw9636@gmail.com>
2017-08-31 11:54:34 -0700

    Add files via upload

--- P4_PIDController/src/PID.cpp
+++ P4_PIDController/src/PID.cpp
@@ -1,110 +1,85 @@
 #include "PID.h"
-#include <cmath>
+#include <math.h>
 #include <iostream>
 
 
 using namespace std;
 
 /*
-* TODO: Complete the PID class.
+* Complete the PID class.
 */
 
 PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
-    PID::Kp = Kp;
-    PID::Ki = Ki;
-    PID::Kd = Kd;
-    p_error = d_error = i_error = 0.0;
 
-    // Twiddling parameters
-    yes_i_wanna_twiddle = false;
-    dp = {0.1*Kp,0.1*Kd,0.1*Ki};
-    step = 1;
-    param_index = 2;  // this will wrao back to 0 after the first twiddle loop
-    n_settle_steps = 100;
-    n_eval_steps = 2000;
-    total_error = 0;
-    best_error = std::numeric_limits<double>::max();
-    tried_adding = false; 
-	tried_subtracting = false;
-	
+void PID::Init(double Kp, double Ki, double Kd) {
+//   this->Kp = Kp;
+//   this->Ki = Ki;
+//   this->Kd = Kd;
+  
+  PID::Kp = Kp;
+  PID::Ki = Ki;
+  PID::Kd = Kd;
+  
+  p_error = 0.1;
+  i_error = 0.001;
+  d_error = 0.1;
 }
 
 void PID::UpdateError(double cte) {
-    if (step == 1) {
-        // to get correct initial d_error
-        p_error = cte;
-    }
-    d_error = cte - p_error;
-    p_error = cte;
-    i_error += cte;
+  double p[] = {Kp, Kd};
+  double dp[] = {p_error, d_error};
+  
+  const double thresh = 0.01;
+  
+  // loop for twiddle
+  if (TotalError() > thresh) {
 
-    // update total error only if we're past number of settle steps
-    if (step % (n_settle_steps + n_eval_steps) > n_settle_steps){
-        total_error += pow(cte,2);
+    switch (current_state) {
+      case 0: {
+        p[last_optimize_index] += dp[last_optimize_index];
+        current_state = 1;
+        break;
+      }
+      case 1: {
+        if (fabs(cte) < fabs(best_error)) {
+          best_error = cte;
+          dp[last_optimize_index] *= 1.1;
+          current_state = 3;
+        } else {
+          p[last_optimize_index] -= 2 * dp[last_optimize_index];
+          current_state = 2;
+        }
+        break;
+      }
+      case 2: {
+        if (fabs(cte) < fabs(best_error)) {
+          best_error = cte;
+          dp[last_optimize_index] *= 1.1;
+        } else {
+          p[last_optimize_index] += dp[last_optimize_index];
+          dp[last_optimize_index] *= 0.9;
+        }
+        current_state = 3;
+        break;
+      }
+      case 3: {
+        last_optimize_index = (last_optimize_index + 1) % 2;
+        current_state = 0;
+        break;
+      }
     }
-
-    // last step in twiddle loop... twiddle it?
-    if (yes_i_wanna_twiddle && step % (n_settle_steps + n_eval_steps) == 0){
-        cout << "step: " << step << endl;
-        cout << "total error: " << total_error << endl;
-        cout << "best error: " << best_error << endl;
-        if (total_error < best_error) {
-            cout << "improvement!" << endl;
-            best_error = total_error;
-            if (step !=  n_settle_steps + n_eval_steps) {
-                // don't do this if it's the first time through
-                dp[param_index] *= 1.1;            
-            }
-            // next parameter
-            param_index = (param_index + 1) % 3;
-            tried_adding = tried_subtracting = false;
-        }
-        if (!tried_adding && !tried_subtracting) {
-            // try adding dp[i] to params[i]
-            AddToParameterAtIndex(param_index, dp[param_index]);
-            tried_adding = true;
-        }
-        else if (tried_adding && !tried_subtracting) {
-            // try subtracting dp[i] from params[i]
-            AddToParameterAtIndex(param_index, -2 * dp[param_index]);     
-            tried_subtracting = true;         
-        }
-        else {
-            // set it back, reduce dp[i], move on to next parameter
-            AddToParameterAtIndex(param_index, dp[param_index]);      
-            dp[param_index] *= 0.9;
-            // next parameter
-            param_index = (param_index + 1) % 3;
-            tried_adding = tried_subtracting = false;
-        }
-        total_error = 0;
-        cout << "new parameters" << endl;
-        cout << "P: " << Kp << ", I: " << Ki << ", D: " << Kd << endl;        
-    }
-	step++;
-
+    cout << "P values " << p[0] << " "<< p[1]  << endl;
+    p_error = dp[0];
+    d_error = dp[1];
+    Kp = p[0];
+    Kd = p[1];
+  }
 }
 
 double PID::TotalError() {
-	return 0;
-}
-
-void PID::AddToParameterAtIndex(int index, double amount) {
-    if (index == 0) {
-        Kp += amount;
-    }
-    else if (index == 1) {
-        Kd += amount;
-    }
-    else if (index == 2) {
-        Ki += amount;
-    }
-    else {
-        std::cout << "AddToParameterAtIndex: index out of bounds";
-    }
+  return fabs(p_error) + fabs(i_error) + fabs(d_error);
 }
 
